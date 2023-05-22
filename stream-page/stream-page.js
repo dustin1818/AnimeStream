a802ad2971dec8674a58f59e13546608021943a6
dustin1818 <dustinamoda18@gmail.com>
2023-05-22 22:11:02 +0800

    update streaming video folder, fix pause and play button bug

--- stream-page/stream-page.js
+++ stream-page/stream-page.js
@@ -1,4 +1,5 @@
-const player = document.getElementById("my-video");
+const player = videojs("my-video");
+
 const ep_id = JSON.parse(localStorage.getItem("ep_id"));
 console.log(ep_id);
 
@@ -7,69 +8,92 @@ const nav_mobile = document.querySelector(".nav-mobile");
 nav.addEventListener("click", () => {
   nav.classList.toggle("is-active");
   nav_mobile.classList.toggle("nav-mobile-active");
-})
+});
 
 //quality button
-// const lowQualityBtn = document.getElementById("360p");
-// const semilowQualityBtn = document.getElementById("480p");
-// const mediumQualityBtn = document.getElementById("720p");
-// const highQualityBtn = document.getElementById("1080p");
+const lowQualityBtn = document.getElementById("360p");
+const semilowQualityBtn = document.getElementById("480p");
+const mediumQualityBtn = document.getElementById("720p");
+const highQualityBtn = document.getElementById("1080p");
+const autoQualityBtn = document.getElementById("auto");
 
 //server button
 const Vidstreaming = document.getElementById("Vidstreaming");
 const GogoServer = document.getElementById("GogoServer");
 const Streamsb = document.getElementById("Streamsb");
-const Xstreamcdn = document.getElementById("Xstreamcdn");
-const Mp4Upload = document.getElementById("Mp4Upload");
-const Doodstream = document.getElementById("Doodstream");
 
 const goBack = () => {
-    window.location.href = "../anime-details/anime-details.html";
-}
+  window.location.href = "../anime-details/anime-details.html";
+};
+
+player.ready(function() {
+  let controlBar = this.controlBar;
+  this.on('play', function() {
+    // Remove the "paused" class from the play toggle button
+    controlBar.playToggle.removeClass('vjs-paused');
+    // Add the "playing" class to the play toggle button
+    controlBar.playToggle.addClass('vjs-playing');
+  });
+
+  this.on('pause', function() {
+    // Remove the "playing" class from the play toggle button
+    controlBar.playToggle.removeClass('vjs-playing');
+    // Add the "paused" class to the play toggle button
+    controlBar.playToggle.addClass('vjs-paused');
+  });
+});
 
 const getAnimeVideoEp = async () => {
+  try {
     let episodeName = ep_id.id;
     console.log(episodeName);
-    // let streamSelections = "vidcloud";
-    try{
-        const {data} = await axios.get(`https://c.delusionz.xyz/anime/gogoanime/servers/${episodeName}`);
-        console.log(data);
+    let streamSelections = "vidstreaming";
+    Vidstreaming.addEventListener("click", () => {
+      streamSelections = "vidstreaming";
+    });
+    GogoServer.addEventListener("click", () => {
+      streamSelections = "gogocdn";
+    });
+    Streamsb.addEventListener("click", () => {
+      streamSelections = "streamsb";
+    });
+    const { data } = await axios.get(
+      `https://c.delusionz.xyz/anime/gogoanime/watch/${episodeName}?server=${streamSelections}`
+    );
 
-         const vidstream = data.find(vidstream => vidstream.name === "Vidstreaming")
-        //button function for vidstreaming
-         Vidstreaming.addEventListener("click", () => {
-          vidstream ? window.location.href = vidstream.url : alert('Server Error, Choose another one.')
-         })
-         const gogostream = data.find(gogostream => gogostream.name === "Gogo server")
-        //button function for gogostream
-        GogoServer.addEventListener("click", () => {
-          gogostream ? window.location.href = gogostream.url : alert('Server Error, Choose another one.')
-         })
-         const streamsbstream = data.find(streamsbstream => streamsbstream.name === "Streamsb")
-        //button function for streamsbstream
-        Streamsb.addEventListener("click", () => {
-          streamsbstream ? window.location.href = streamsbstream.url : alert('Server Error, Choose another one.')
-         })
-         const xstreamcdnstream = data.find(xstreamcdnstream => xstreamcdnstream.name === "Xstreamcdn")
-        //button function for xstreamcdnstream
-        Xstreamcdn.addEventListener("click", () => {
-          xstreamcdnstream ? window.location.href = xstreamcdnstream.url : alert('Server Error, Choose another one.')
-         })
-         const mp4uploadstream = data.find(mp4uploadstream => mp4uploadstream.name === "Mp4Upload")
-        //button function for mp4uploadstream
-        Mp4Upload.addEventListener("click", () => {
-          mp4uploadstream ? window.location.href = mp4uploadstream.url : alert('Server Error, Choose another one.')
-         })
-         const doodstream = data.find(doodstream => doodstream.name === "Doodstream")
-        //button function for doodstream
-        Doodstream.addEventListener("click", () => {
-          doodstream ? window.location.href = doodstream.url : alert('Server Error, Choose another one.')
-         })
-    }catch(e){
-        throw new Error(e.message);
-    }
-}
+    //360p
+    lowQualityBtn.addEventListener("click", () => {
+      player.src(`${data.sources[0].url}`);
+      console.log(data, streamSelections);
+    });
 
+    //480p
+    semilowQualityBtn.addEventListener("click", () => {
+      player.src(`${data.sources[1].url}`);
+      console.log(data, streamSelections);
+    });
+
+    //720p
+    mediumQualityBtn.addEventListener("click", () => {
+      player.src(`${data.sources[2].url}`);
+      console.log(data, streamSelections);
+    });
+
+    //1080p
+    highQualityBtn.addEventListener("click", () => {
+      player.src(`${data.sources[3].url}`);
+      console.log(data, streamSelections);
+    });
+
+    //auto
+    autoQualityBtn.addEventListener("click", () => {
+      player.src(`${data.sources[4].url}`);
+      console.log(data, streamSelections);
+    });
+    player.src(`${data.sources[3].url}`);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
 
 getAnimeVideoEp();
-
