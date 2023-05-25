a418fb53b5d59447fba5ffc73d540f61fffa4c85
dustin1818 <dustinamoda18@gmail.com>
2023-05-25 18:27:44 +0800

    temporary fix for video player bug, still broken

--- stream-page/stream-page.js
+++ stream-page/stream-page.js
@@ -26,85 +26,102 @@ const goBack = () => {
   window.location.href = "../anime-details/anime-details.html";
 };
 
-player.ready(function() {
+player.ready(function () {
   let controlBar = this.controlBar;
-  this.on("play", function() {
+  this.on("play", function () {
     controlBar.playToggle.removeClass("vjs-paused");
     controlBar.playToggle.addClass("vjs-playing");
   });
 
-  this.on('timeupdate', function() {
+  this.on("timeupdate", function () {
     const remainingTime = player.remainingTime();
     const remainingTimeString = formatTime(remainingTime);
-    const remainingTimeDisplay = document.querySelector('.vjs-remaining-time-display');
+    const remainingTimeDisplay = document.querySelector(
+      ".vjs-remaining-time-display"
+    );
     remainingTimeDisplay.innerHTML = remainingTimeString;
   });
   //helper function formatter
   function formatTime(timeInSeconds) {
     const hours = Math.floor(timeInSeconds / 3600);
-    const hoursCheck = hours ? hours + ':'  : '' // check if the video length has hours
+    // check if the video length has hours == hoursCheck
+    const hoursCheck = hours ? hours + ":" : "";
     const minutes = Math.floor((timeInSeconds % 3600) / 60);
     const seconds = Math.floor(timeInSeconds % 60);
-  
-    return ('' + hoursCheck).slice(-2) +
-           ('0' + minutes).slice(-2) + ':' +
-           ('0' + seconds).slice(-2);
+
+    return (
+      ("" + hoursCheck).slice(-2) +
+      ("0" + minutes).slice(-2) +
+      ":" +
+      ("0" + seconds).slice(-2)
+    );
   }
 
-  this.on("pause", function() {
+  this.on("pause", function () {
     controlBar.playToggle.removeClass("vjs-playing");
     controlBar.playToggle.addClass("vjs-paused");
   });
 });
 
+Vidstreaming.addEventListener("click", () => {
+  streamSelections = "vidstreaming";
+  alert("Choose a video quality");
+});
+GogoServer.addEventListener("click", () => {
+  streamSelections = "gogocdn";
+  alert("Choose a video quality");
+});
+Streamsb.addEventListener("click", () => {
+  streamSelections = "streamsb";
+  player.currentTime(0);
+  alert("Choose a video quality");
+});
 
 const getAnimeVideoEp = async () => {
   try {
     let episodeName = ep_id.id;
     console.log(episodeName);
     let streamSelections = "vidstreaming";
-    Vidstreaming.addEventListener("click", () => {
-      streamSelections = "vidstreaming";
-    });
-    GogoServer.addEventListener("click", () => {
-      streamSelections = "gogocdn";
-    });
-    Streamsb.addEventListener("click", () => {
-      streamSelections = "streamsb";
-    });
     const { data } = await axios.get(
       `https://c.delusionz.xyz/anime/gogoanime/watch/${episodeName}?server=${streamSelections}`
     );
 
     //360p
     lowQualityBtn.addEventListener("click", () => {
+      alert('Updating quality');
+      player.currentTime(0);
       player.src(`${data.sources[0].url}`);
-      console.log(data, streamSelections);
     });
 
     //480p
     semilowQualityBtn.addEventListener("click", () => {
+      alert('Updating quality');
+      player.currentTime(0);
       player.src(`${data.sources[1].url}`);
-      console.log(data, streamSelections);
     });
 
     //720p
     mediumQualityBtn.addEventListener("click", () => {
+      alert('Updating quality');
+      player.currentTime(0);
       player.src(`${data.sources[2].url}`);
-      console.log(data, streamSelections);
     });
 
     //1080p
     highQualityBtn.addEventListener("click", () => {
+      alert('Updating quality');
+      player.currentTime(0);
       player.src(`${data.sources[3].url}`);
-      console.log(data, streamSelections);
     });
 
     //auto
     autoQualityBtn.addEventListener("click", () => {
+      alert('Updating quality');
+      player.currentTime(0);
       player.src(`${data.sources[4].url}`);
-      console.log(data, streamSelections);
     });
+    alert('Video will start playing');
+    player.currentTime(0);
     player.src(`${data.sources[3].url}`);
   } catch (e) {
     throw new Error(e.message);
